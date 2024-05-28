1e96e2d3fd95b53647aafdff22e63d56c689f7be
keilylopez22 <klopezh16@miumg.edu.gt>
2024-05-27 19:08:42 -0600

    Profesores

---
+++ lmsProyectoFinal/FRMProfesores.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace lmsProyectoFinal
+{
+    partial class FRMProfesores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProfesores = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProfesores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProfesores
+            // 
+            this.dgvProfesores.AllowUserToAddRows = false;
+            this.dgvProfesores.AllowUserToDeleteRows = false;
+            this.dgvProfesores.AllowUserToOrderColumns = true;
+            this.dgvProfesores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProfesores.Location = new System.Drawing.Point(12, 246);
+            this.dgvProfesores.Name = "dgvProfesores";
+            this.dgvProfesores.ReadOnly = true;
+            this.dgvProfesores.RowHeadersWidth = 51;
+            this.dgvProfesores.RowTemplate.Height = 24;
+            this.dgvProfesores.Size = new System.Drawing.Size(753, 150);
+            this.dgvProfesores.TabIndex = 0;
+            // 
+            // FRMProfesores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProfesores);
+            this.Name = "FRMProfesores";
+            this.Text = "FRMProfesores";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProfesores)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProfesores;
+    }
+}
---
+++ lmsProyectoFinal/FRMProfesores.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lmsProyectoFinal
+{
+    public partial class FRMProfesores : Form
+    {
+        ProfesorDAO profesorDAO = new ProfesorDAO();
+        public FRMProfesores()
+        {
+            InitializeComponent();
+            listarProfesores();
+        }
+
+        private void listarProfesores()
+        {
+            dgvProfesores.DataSource = profesorDAO.GetAllProfesores();
+        }
+    }
+}
