af2b95181b08dacb3e0730c5c5f483d04bee5039
keilylopez22 <klopezh16@miumg.edu.gt>
2024-05-27 19:09:52 -0600

    Actividades

---
+++ lmsProyectoFinal/FRMActividades.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace lmsProyectoFinal
+{
+    partial class FRMActividades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvActividades = new System.Windows.Forms.DataGridView();
+            this.cmbCursos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Descripcion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.datePickFechaEntrega = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvActividades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvActividades
+            // 
+            this.dgvActividades.AllowUserToAddRows = false;
+            this.dgvActividades.AllowUserToDeleteRows = false;
+            this.dgvActividades.AllowUserToOrderColumns = true;
+            this.dgvActividades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvActividades.Location = new System.Drawing.Point(50, 337);
+            this.dgvActividades.Name = "dgvActividades";
+            this.dgvActividades.ReadOnly = true;
+            this.dgvActividades.RowHeadersWidth = 51;
+            this.dgvActividades.RowTemplate.Height = 24;
+            this.dgvActividades.Size = new System.Drawing.Size(683, 150);
+            this.dgvActividades.TabIndex = 0;
+            this.dgvActividades.SelectionChanged += new System.EventHandler(this.dgvActividades_SelectionChanged);
+            // 
+            // cmbCursos
+            // 
+            this.cmbCursos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCursos.FormattingEnabled = true;
+            this.cmbCursos.Location = new System.Drawing.Point(444, 36);
+            this.cmbCursos.Name = "cmbCursos";
+            this.cmbCursos.Size = new System.Drawing.Size(192, 28);
+            this.cmbCursos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(439, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Curso";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitulo.Location = new System.Drawing.Point(50, 38);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(366, 26);
+            this.txtTitulo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Titulo";
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.AutoSize = true;
+            this.Descripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Descripcion.Location = new System.Drawing.Point(46, 122);
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.Size = new System.Drawing.Size(99, 20);
+            this.Descripcion.TabIndex = 5;
+            this.Descripcion.Text = "Descripcion";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.Location = new System.Drawing.Point(50, 145);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(683, 117);
+            this.txtDescripcion.TabIndex = 6;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(433, 278);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(122, 37);
+            this.btnCancelar.TabIndex = 17;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.FlatAppearance.BorderSize = 0;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(135, 278);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(101, 37);
+            this.btnGuardar.TabIndex = 16;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // datePickFechaEntrega
+            // 
+            this.datePickFechaEntrega.Location = new System.Drawing.Point(50, 97);
+            this.datePickFechaEntrega.Name = "datePickFechaEntrega";
+            this.datePickFechaEntrega.Size = new System.Drawing.Size(200, 22);
+            this.datePickFechaEntrega.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 20);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Fecha Entrega";
+            // 
+            // FRMActividades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.datePickFechaEntrega);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.Descripcion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCursos);
+            this.Controls.Add(this.dgvActividades);
+            this.Name = "FRMActividades";
+            this.Text = "FRMActividades";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvActividades)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvActividades;
+        private System.Windows.Forms.ComboBox cmbCursos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Descripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DateTimePicker datePickFechaEntrega;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ lmsProyectoFinal/FRMActividades.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lmsProyectoFinal
+{
+    public partial class FRMActividades : Form
+    {
+
+        bool esNuevo = true;
+        ActividadDAO actividadDAO = new ActividadDAO();
+        CursoDAO cursoDAO = new CursoDAO();
+        Actividad actividadSeleccionada;
+        public FRMActividades()
+        {
+            InitializeComponent();
+
+            listarCursos();
+            ListarActividades();
+        }
+
+        public void ListarActividades()
+        {
+            int cursoId = -1;
+            if (cmbCursos.SelectedValue!=null)
+                cursoId = (int)cmbCursos.SelectedValue;
+            dgvActividades.DataSource= actividadDAO.GetAllActividades(cursoId);
+            dgvActividades.Columns["Id"].Visible= false;
+            dgvActividades.Columns["cursoId"].Visible = false;
+        }
+
+        public void listarCursos()
+        {
+            List<Curso> lista = new List<Curso>();
+            lista.Add(new Curso { 
+            Id = -1,
+            Nombre = "--Seleccionar--"
+            });
+            lista.AddRange(cursoDAO.GetAllCursos());
+            cmbCursos.DataSource= lista;
+            cmbCursos.DisplayMember= "Nombre";
+            cmbCursos.ValueMember = "Id";
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (esNuevo)
+            {
+                guardarNuevo();
+            }
+            else
+            {
+                actualizar();
+            }
+            limpiarCampos();
+            esNuevo = true;
+            ListarActividades();
+        }
+
+        private void guardarNuevo()
+        {
+            Actividad actividad = new Actividad
+            {
+                CursoId = (int)cmbCursos.SelectedValue,
+                Titulo = txtTitulo.Text,
+                Descripcion = txtDescripcion.Text,
+                FechaEntrega = datePickFechaEntrega.Value
+            };
+            actividadDAO.AddActividad(actividad);
+
+        }
+
+        private void actualizar()
+        {
+            actividadSeleccionada.Titulo = txtTitulo.Text;
+            actividadSeleccionada.Descripcion = txtDescripcion.Text;
+            actividadSeleccionada.FechaEntrega=datePickFechaEntrega.Value;
+            actividadSeleccionada.CursoId = (int)cmbCursos.SelectedValue;
+            actividadDAO.UpdateActividad(actividadSeleccionada);
+        }
+
+        private void limpiarCampos()
+        {
+            txtDescripcion.Clear();
+            txtTitulo.Clear();
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpiarCampos();
+            esNuevo= true;
+        }
+
+        private void dgvActividades_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvActividades.SelectedRows.Count > 0) {
+                DataGridViewRow row = dgvActividades.SelectedRows[0];
+                actividadSeleccionada = new Actividad
+                {
+                    Id = (int)row.Cells["id"].Value,
+                    CursoId = (int)row.Cells["CursoId"].Value,
+                    Titulo = (string)row.Cells["Titulo"].Value,
+                    Descripcion = (string)row.Cells["Descripcion"].Value,
+                    FechaEntrega = (DateTime)row.Cells["FechaEntrega"].Value,
+                    Curso = (String)row.Cells["Curso"].Value,
+                };
+                txtTitulo.Text = actividadSeleccionada.Titulo;
+                cmbCursos.SelectedValue = actividadSeleccionada.CursoId;
+                txtDescripcion.Text = actividadSeleccionada.Descripcion;
+                datePickFechaEntrega.Value = actividadSeleccionada.FechaEntrega;
+                esNuevo = false;
+            }
+        }
+    }
+}
