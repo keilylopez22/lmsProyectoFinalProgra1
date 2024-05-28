12b46ceca8cbf997105b3a05dd3fe4a4e160cff1
keilylopez22 <klopezh16@miumg.edu.gt>
2024-05-28 11:16:14 -0600

    guardar nuevo y actualizar curso

--- lmsProyectoFinal/FRMCursos.Designer.cs
+++ lmsProyectoFinal/FRMCursos.Designer.cs
@@ -29,6 +29,16 @@
         private void InitializeComponent()
         {
             this.dgvCursos = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpkFechaIni = new System.Windows.Forms.DateTimePicker();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.Descripcion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpkFechaFin = new System.Windows.Forms.DateTimePicker();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCursos)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,30 +48,149 @@
             this.dgvCursos.AllowUserToDeleteRows = false;
             this.dgvCursos.AllowUserToOrderColumns = true;
             this.dgvCursos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCursos.Location = new System.Drawing.Point(95, 249);
+            this.dgvCursos.Location = new System.Drawing.Point(95, 363);
             this.dgvCursos.Name = "dgvCursos";
             this.dgvCursos.ReadOnly = true;
             this.dgvCursos.RowHeadersWidth = 51;
             this.dgvCursos.RowTemplate.Height = 24;
             this.dgvCursos.Size = new System.Drawing.Size(857, 150);
             this.dgvCursos.TabIndex = 0;
+            this.dgvCursos.SelectionChanged += new System.EventHandler(this.dgvCursos_SelectionChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(91, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Fecha Inicio";
+            // 
+            // dtpkFechaIni
+            // 
+            this.dtpkFechaIni.Location = new System.Drawing.Point(95, 91);
+            this.dtpkFechaIni.Name = "dtpkFechaIni";
+            this.dtpkFechaIni.Size = new System.Drawing.Size(200, 22);
+            this.dtpkFechaIni.TabIndex = 24;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.Location = new System.Drawing.Point(95, 139);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(683, 117);
+            this.txtDescripcion.TabIndex = 23;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.AutoSize = true;
+            this.Descripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Descripcion.Location = new System.Drawing.Point(91, 116);
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.Size = new System.Drawing.Size(99, 20);
+            this.Descripcion.TabIndex = 22;
+            this.Descripcion.Text = "Descripcion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(95, 32);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(366, 26);
+            this.txtNombre.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(385, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Fecha Fin";
+            // 
+            // dtpkFechaFin
+            // 
+            this.dtpkFechaFin.Location = new System.Drawing.Point(389, 91);
+            this.dtpkFechaFin.Name = "dtpkFechaFin";
+            this.dtpkFechaFin.Size = new System.Drawing.Size(200, 22);
+            this.dtpkFechaFin.TabIndex = 26;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(506, 284);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(122, 37);
+            this.btnCancelar.TabIndex = 29;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.FlatAppearance.BorderSize = 0;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(208, 284);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(101, 37);
+            this.btnGuardar.TabIndex = 28;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // FRMCursos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(1112, 450);
+            this.ClientSize = new System.Drawing.Size(1112, 543);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpkFechaFin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpkFechaIni);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.Descripcion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.dgvCursos);
             this.Name = "FRMCursos";
             this.Text = "FRMCursos";
             ((System.ComponentModel.ISupportInitialize)(this.dgvCursos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvCursos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpkFechaIni;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label Descripcion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpkFechaFin;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
     }
 }
--- lmsProyectoFinal/FRMCursos.cs
+++ lmsProyectoFinal/FRMCursos.cs
@@ -15,6 +15,8 @@ namespace lmsProyectoFinal
     {
 
         CursoDAO cursoDAO = new CursoDAO();
+        Curso cursoSeleccionado;
+        bool esNuevo = true;
         public FRMCursos()
         {
             InitializeComponent();
@@ -24,6 +26,82 @@ namespace lmsProyectoFinal
         private void listarCursos()
         {
             dgvCursos.DataSource= cursoDAO.GetAllCursos();
+        }
+
+
+        private void guardarNuevo()
+        {
+            Curso curso = new Curso
+            {
+                Nombre = txtNombre.Text,
+                Descripcion = txtDescripcion.Text,
+                FechaInicio = dtpkFechaIni.Value,
+                FechaFin = dtpkFechaFin.Value
+            };
+            cursoDAO.AddCurso(curso);
+
+        }
+
+        private void actualizar()
+        {
+            if (cursoSeleccionado != null)
+            {
+                cursoSeleccionado.Nombre= txtNombre.Text;
+                cursoSeleccionado.Descripcion = txtDescripcion.Text;
+                cursoSeleccionado.FechaInicio= dtpkFechaIni.Value;
+                cursoSeleccionado.FechaFin= dtpkFechaFin.Value;
+                cursoDAO.UpdateCurso(cursoSeleccionado);
+                cursoSeleccionado= null;
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (esNuevo)
+            {
+                guardarNuevo();
+            }
+            else
+            {
+                actualizar();
+            }
+            listarCursos();
+            reiniciarCampos();
+            esNuevo = true;
+        }
+
+        private void reiniciarCampos()
+        {
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            reiniciarCampos();
+            cursoSeleccionado = null;
+            esNuevo= true;
+        }
+
+        private void dgvCursos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvCursos.SelectedRows.Count > 0)
+            {
+                DataGridViewRow row = dgvCursos.SelectedRows[0];
+                cursoSeleccionado = new Curso
+                {
+                    Id = (int)row.Cells["id"].Value,
+                    Nombre = (string)row.Cells["Nombre"].Value,
+                    Descripcion = (string)row.Cells["Descripcion"].Value,
+                    FechaInicio = (DateTime)row.Cells["FechaInicio"].Value,
+                    FechaFin = (DateTime)row.Cells["FechaFin"].Value
+                };
+                txtNombre.Text= cursoSeleccionado.Nombre;
+                txtDescripcion.Text= cursoSeleccionado.Descripcion;
+                dtpkFechaFin.Value = cursoSeleccionado.FechaFin;
+                dtpkFechaIni.Value = cursoSeleccionado.FechaInicio;
+                esNuevo = false;
+            }
         }
     }
 }
