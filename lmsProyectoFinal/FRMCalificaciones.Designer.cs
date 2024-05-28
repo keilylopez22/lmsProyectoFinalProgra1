d9f7d75220f372f59ab09b85d6b8cd04af2528fc
keilylopez22 <klopezh16@miumg.edu.gt>
2024-05-27 19:09:10 -0600

    Informe Calificaciones

---
+++ lmsProyectoFinal/FRMCalificaciones.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace lmsProyectoFinal
+{
+    partial class FRMCalificaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCalificaciones = new System.Windows.Forms.DataGridView();
+            this.cmbCursos = new System.Windows.Forms.ComboBox();
+            this.cmbEstudiante = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEstudiante = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.numNota = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCalificaciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCalificaciones
+            // 
+            this.dgvCalificaciones.AllowUserToAddRows = false;
+            this.dgvCalificaciones.AllowUserToDeleteRows = false;
+            this.dgvCalificaciones.AllowUserToOrderColumns = true;
+            this.dgvCalificaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCalificaciones.Location = new System.Drawing.Point(38, 272);
+            this.dgvCalificaciones.Name = "dgvCalificaciones";
+            this.dgvCalificaciones.ReadOnly = true;
+            this.dgvCalificaciones.RowHeadersWidth = 51;
+            this.dgvCalificaciones.RowTemplate.Height = 24;
+            this.dgvCalificaciones.Size = new System.Drawing.Size(931, 150);
+            this.dgvCalificaciones.TabIndex = 0;
+            this.dgvCalificaciones.SelectionChanged += new System.EventHandler(this.dgvCalificaciones_SelectionChanged);
+            // 
+            // cmbCursos
+            // 
+            this.cmbCursos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCursos.FormattingEnabled = true;
+            this.cmbCursos.Location = new System.Drawing.Point(38, 229);
+            this.cmbCursos.Name = "cmbCursos";
+            this.cmbCursos.Size = new System.Drawing.Size(225, 28);
+            this.cmbCursos.TabIndex = 1;
+            // 
+            // cmbEstudiante
+            // 
+            this.cmbEstudiante.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbEstudiante.FormattingEnabled = true;
+            this.cmbEstudiante.Location = new System.Drawing.Point(283, 229);
+            this.cmbEstudiante.Name = "cmbEstudiante";
+            this.cmbEstudiante.Size = new System.Drawing.Size(225, 28);
+            this.cmbEstudiante.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(548, 220);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(101, 37);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Curso:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(288, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Estudiante:";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(38, 37);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.ReadOnly = true;
+            this.txtTitulo.Size = new System.Drawing.Size(370, 22);
+            this.txtTitulo.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Titulo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(461, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Estudiante";
+            // 
+            // txtEstudiante
+            // 
+            this.txtEstudiante.Location = new System.Drawing.Point(465, 37);
+            this.txtEstudiante.Name = "txtEstudiante";
+            this.txtEstudiante.Size = new System.Drawing.Size(370, 22);
+            this.txtEstudiante.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(896, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Nota";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.FlatAppearance.BorderSize = 0;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(229, 118);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(101, 37);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(527, 118);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(122, 37);
+            this.btnCancelar.TabIndex = 15;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(34, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Titulo";
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(38, 85);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.ReadOnly = true;
+            this.txtArchivo.Size = new System.Drawing.Size(786, 22);
+            this.txtArchivo.TabIndex = 16;
+            // 
+            // numNota
+            // 
+            this.numNota.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numNota.Location = new System.Drawing.Point(841, 33);
+            this.numNota.Name = "numNota";
+            this.numNota.Size = new System.Drawing.Size(159, 74);
+            this.numNota.TabIndex = 18;
+            // 
+            // FRMCalificaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1079, 450);
+            this.Controls.Add(this.numNota);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEstudiante);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.cmbEstudiante);
+            this.Controls.Add(this.cmbCursos);
+            this.Controls.Add(this.dgvCalificaciones);
+            this.Name = "FRMCalificaciones";
+            this.Text = "FRMCalificaciones";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCalificaciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCalificaciones;
+        private System.Windows.Forms.ComboBox cmbCursos;
+        private System.Windows.Forms.ComboBox cmbEstudiante;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEstudiante;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.TextBox numNota;
+    }
+}
---
+++ lmsProyectoFinal/FRMCalificaciones.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lmsProyectoFinal
+{
+    public partial class FRMCalificaciones : Form
+    {
+        EntregaDAO entregaDao = new EntregaDAO();
+        CursoDAO cursoDAO = new CursoDAO();
+        EstudianteDAO estudianteDAO = new EstudianteDAO();
+
+        private Entrega entregaSeleccionada;
+        public FRMCalificaciones()
+        {
+            InitializeComponent();
+            listarCursos();
+            listarEstudiantes();
+            listarCalificaciones();
+        }
+
+        private void listarCalificaciones()
+        {
+            int idEstudiante = (int)cmbEstudiante.SelectedValue;
+            int idCurso = (int)cmbCursos.SelectedValue;
+            dgvCalificaciones.DataSource = entregaDao.GetAllEntregas(idCurso, idEstudiante);
+            dgvCalificaciones.Columns["Id"].Visible = false;
+            dgvCalificaciones.Columns["ActividadId"].Visible = false;
+            dgvCalificaciones.Columns["EstudianteId"].Visible = false;
+            dgvCalificaciones.Columns["Estudiante"].DisplayIndex = 0;
+            dgvCalificaciones.Columns["Titulo"].DisplayIndex = 1;
+            dgvCalificaciones.Columns["Curso"].DisplayIndex = 2;
+        }
+
+        public void listarCursos()
+        {
+            List<Curso> lista = new List<Curso>();
+            lista.Add(new Curso
+            {
+                Id = -1,
+                Nombre = "--Seleccionar--"
+            });
+            lista.AddRange(cursoDAO.GetAllCursos());
+            cmbCursos.DataSource = lista;
+            cmbCursos.DisplayMember = "Nombre";
+            cmbCursos.ValueMember = "Id";
+        }
+
+        public void listarEstudiantes()
+        {
+            List<Estudiante> lista = new List<Estudiante>();
+            lista.Add(new Estudiante
+            {
+                Id = -1,
+                Nombre = "--Seleccionar--"
+            });
+            lista.AddRange(estudianteDAO.GetAllEstudiantes());
+            cmbEstudiante.DataSource = lista;
+            cmbEstudiante.DisplayMember = "Nombre";
+            cmbEstudiante.ValueMember = "Id";
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            listarCalificaciones();
+        }
+
+        private void HabDeshabCampos(bool habilitar)
+        {
+            txtTitulo.ReadOnly = habilitar;
+            txtEstudiante.ReadOnly = habilitar;
+            numNota.ReadOnly = habilitar;
+        }
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            numNota.ReadOnly = false;
+        }
+
+        private void inicializarCampos()
+        {
+            txtTitulo.Clear();
+            txtEstudiante.Clear();
+            numNota.Text= "0";
+            HabDeshabCampos(true);
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            inicializarCampos();
+        }
+
+        private void dgvCalificaciones_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvCalificaciones.SelectedRows.Count > 0)
+            {
+                DataGridViewRow row = dgvCalificaciones.SelectedRows[0];
+                entregaSeleccionada = new Entrega
+                {
+                    Id = (int)row.Cells["Id"].Value,
+                    ActividadId = (int)row.Cells["ActividadId"].Value,
+                    EstudianteId = (int)row.Cells["EstudianteId"].Value,
+                    Calificacion = (decimal)row.Cells["Calificacion"].Value,
+                    ArchivoUrl = (string)row.Cells["ArchivoUrl"].Value,
+                    FechaEntrega = (DateTime)row.Cells["FechaEntrega"].Value
+                };
+                txtEstudiante.Text = (string)row.Cells["Estudiante"].Value;
+                txtTitulo.Text = (string)row.Cells["Titulo"].Value;
+                txtArchivo.Text = (string)row.Cells["ArchivoUrl"].Value;
+                numNota.Text = ((decimal)row.Cells["Calificacion"].Value).ToString();
+            }            
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (null != entregaSeleccionada)
+            {
+                entregaSeleccionada.Calificacion = Convert.ToDecimal(numNota.Text);
+                entregaDao.UpdateEntrega(entregaSeleccionada);
+            }
+        }
+    }
+
+
+}
