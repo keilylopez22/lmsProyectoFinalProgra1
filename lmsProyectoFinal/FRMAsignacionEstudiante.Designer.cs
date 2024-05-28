622c1c669e1219f7f148e5f5a72036fb638576da
keilylopez22 <klopezh16@miumg.edu.gt>
2024-05-27 19:09:42 -0600

    Asignacion de Estudiantes a Cursos

---
+++ lmsProyectoFinal/FRMAsignacionEstudiante.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace lmsProyectoFinal
+{
+    partial class FRMAsignacionEstudiante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAsignacion = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsignacion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAsignacion
+            // 
+            this.dgvAsignacion.AllowUserToAddRows = false;
+            this.dgvAsignacion.AllowUserToDeleteRows = false;
+            this.dgvAsignacion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAsignacion.Location = new System.Drawing.Point(32, 267);
+            this.dgvAsignacion.Name = "dgvAsignacion";
+            this.dgvAsignacion.ReadOnly = true;
+            this.dgvAsignacion.RowHeadersWidth = 51;
+            this.dgvAsignacion.RowTemplate.Height = 24;
+            this.dgvAsignacion.Size = new System.Drawing.Size(718, 150);
+            this.dgvAsignacion.TabIndex = 1;
+            // 
+            // FRMAsignacionEstudiante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvAsignacion);
+            this.Name = "FRMAsignacionEstudiante";
+            this.Text = "FRMAsignacionEstudiante";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsignacion)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAsignacion;
+    }
+}
---
+++ lmsProyectoFinal/FRMAsignacionEstudiante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lmsProyectoFinal
+{
+    public partial class FRMAsignacionEstudiante : Form
+    {
+        EstudianteDAO estudianteDAO = new EstudianteDAO();
+        public FRMAsignacionEstudiante()
+        {
+            InitializeComponent();
+            listarAsignaciones();
+        }
+
+        private void listarAsignaciones()
+        {
+            dgvAsignacion.DataSource = estudianteDAO.GetAllAsignaciones();
+            dgvAsignacion.Columns["id"].Visible= false;
+            dgvAsignacion.Columns["CursoId"].Visible = false;
+            dgvAsignacion.Columns["EstudianteId"].Visible = false;
+
+        }
+    }
+}
