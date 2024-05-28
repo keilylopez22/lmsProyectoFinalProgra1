51fd658005c2fe2a841991a389e004ba82c71590
keilylopez22 <klopezh16@miumg.edu.gt>
2024-05-27 19:08:52 -0600

    Cursos

---
+++ lmsProyectoFinal/FRMCursos.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace lmsProyectoFinal
+{
+    partial class FRMCursos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCursos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCursos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCursos
+            // 
+            this.dgvCursos.AllowUserToAddRows = false;
+            this.dgvCursos.AllowUserToDeleteRows = false;
+            this.dgvCursos.AllowUserToOrderColumns = true;
+            this.dgvCursos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCursos.Location = new System.Drawing.Point(95, 249);
+            this.dgvCursos.Name = "dgvCursos";
+            this.dgvCursos.ReadOnly = true;
+            this.dgvCursos.RowHeadersWidth = 51;
+            this.dgvCursos.RowTemplate.Height = 24;
+            this.dgvCursos.Size = new System.Drawing.Size(857, 150);
+            this.dgvCursos.TabIndex = 0;
+            // 
+            // FRMCursos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1112, 450);
+            this.Controls.Add(this.dgvCursos);
+            this.Name = "FRMCursos";
+            this.Text = "FRMCursos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCursos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCursos;
+    }
+}
---
+++ lmsProyectoFinal/FRMCursos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lmsProyectoFinal
+{
+
+    public partial class FRMCursos : Form
+    {
+
+        CursoDAO cursoDAO = new CursoDAO();
+        public FRMCursos()
+        {
+            InitializeComponent();
+            listarCursos();
+        }
+
+        private void listarCursos()
+        {
+            dgvCursos.DataSource= cursoDAO.GetAllCursos();
+        }
+    }
+}
