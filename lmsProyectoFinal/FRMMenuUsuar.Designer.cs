3388cdccc7b5c27772b5ea259078c46ccc247d7a
keilylopez22 <keilysamanta125@gmail.com>
2024-05-29 02:11:58 -0600

    Usuarios

--- lmsProyectoFinal/FRMMenuUsuar.Designer.cs
+++ lmsProyectoFinal/FRMMenuUsuar.Designer.cs
@@ -28,38 +28,321 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRMMenuUsuar));
             this.pnlContenedor = new System.Windows.Forms.Panel();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.pnlSubMenuCursos = new System.Windows.Forms.Panel();
+            this.btnListarUsuarios = new System.Windows.Forms.Button();
+            this.btnEstudiantes = new System.Windows.Forms.Button();
+            this.pnlSubMenuEstudiantes = new System.Windows.Forms.Panel();
+            this.btnListarEstudent = new System.Windows.Forms.Button();
+            this.btnAsignarEstudiantes = new System.Windows.Forms.Button();
+            this.btnProfresores = new System.Windows.Forms.Button();
+            this.pnlSubMenuProfesores = new System.Windows.Forms.Panel();
+            this.btnListarProfesores = new System.Windows.Forms.Button();
+            this.btnAsignarProfesores = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnListarCursos = new System.Windows.Forms.Button();
+            this.btnActividaes = new System.Windows.Forms.Button();
+            this.btnInformes = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCalificaciones = new System.Windows.Forms.Button();
             this.pnlSubMenu = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pnlContenedor.SuspendLayout();
+            this.pnlLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlSubMenuCursos.SuspendLayout();
+            this.pnlSubMenuEstudiantes.SuspendLayout();
+            this.pnlSubMenuProfesores.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.pnlSubMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlContenedor
             // 
+            this.pnlContenedor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.pnlContenedor.Controls.Add(this.pictureBox2);
             this.pnlContenedor.Controls.Add(this.pnlSubMenu);
             this.pnlContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlContenedor.Location = new System.Drawing.Point(0, 0);
             this.pnlContenedor.Name = "pnlContenedor";
-            this.pnlContenedor.Size = new System.Drawing.Size(1039, 563);
+            this.pnlContenedor.Size = new System.Drawing.Size(1241, 663);
             this.pnlContenedor.TabIndex = 0;
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.Controls.Add(this.pictureBox1);
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(200, 100);
+            this.pnlLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsuarios.Location = new System.Drawing.Point(0, 100);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(200, 40);
+            this.btnUsuarios.TabIndex = 1;
+            this.btnUsuarios.Text = "Usuarios";
+            this.btnUsuarios.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // pnlSubMenuCursos
+            // 
+            this.pnlSubMenuCursos.Controls.Add(this.btnListarUsuarios);
+            this.pnlSubMenuCursos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlSubMenuCursos.Location = new System.Drawing.Point(0, 140);
+            this.pnlSubMenuCursos.Name = "pnlSubMenuCursos";
+            this.pnlSubMenuCursos.Size = new System.Drawing.Size(200, 62);
+            this.pnlSubMenuCursos.TabIndex = 2;
+            // 
+            // btnListarUsuarios
+            // 
+            this.btnListarUsuarios.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnListarUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListarUsuarios.Location = new System.Drawing.Point(0, 0);
+            this.btnListarUsuarios.Name = "btnListarUsuarios";
+            this.btnListarUsuarios.Size = new System.Drawing.Size(200, 39);
+            this.btnListarUsuarios.TabIndex = 0;
+            this.btnListarUsuarios.Text = "Listar";
+            this.btnListarUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // btnEstudiantes
+            // 
+            this.btnEstudiantes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEstudiantes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEstudiantes.Location = new System.Drawing.Point(0, 202);
+            this.btnEstudiantes.Name = "btnEstudiantes";
+            this.btnEstudiantes.Size = new System.Drawing.Size(200, 40);
+            this.btnEstudiantes.TabIndex = 3;
+            this.btnEstudiantes.Text = "Estudiantes";
+            this.btnEstudiantes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEstudiantes.UseVisualStyleBackColor = true;
+            // 
+            // pnlSubMenuEstudiantes
+            // 
+            this.pnlSubMenuEstudiantes.Controls.Add(this.btnAsignarEstudiantes);
+            this.pnlSubMenuEstudiantes.Controls.Add(this.btnListarEstudent);
+            this.pnlSubMenuEstudiantes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlSubMenuEstudiantes.Location = new System.Drawing.Point(0, 242);
+            this.pnlSubMenuEstudiantes.Name = "pnlSubMenuEstudiantes";
+            this.pnlSubMenuEstudiantes.Size = new System.Drawing.Size(200, 84);
+            this.pnlSubMenuEstudiantes.TabIndex = 4;
+            // 
+            // btnListarEstudent
+            // 
+            this.btnListarEstudent.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnListarEstudent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListarEstudent.Location = new System.Drawing.Point(0, 0);
+            this.btnListarEstudent.Name = "btnListarEstudent";
+            this.btnListarEstudent.Size = new System.Drawing.Size(200, 34);
+            this.btnListarEstudent.TabIndex = 0;
+            this.btnListarEstudent.Text = "Listar";
+            this.btnListarEstudent.UseVisualStyleBackColor = true;
+            // 
+            // btnAsignarEstudiantes
+            // 
+            this.btnAsignarEstudiantes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAsignarEstudiantes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAsignarEstudiantes.Location = new System.Drawing.Point(0, 34);
+            this.btnAsignarEstudiantes.Name = "btnAsignarEstudiantes";
+            this.btnAsignarEstudiantes.Size = new System.Drawing.Size(200, 34);
+            this.btnAsignarEstudiantes.TabIndex = 1;
+            this.btnAsignarEstudiantes.Text = "Asignar";
+            this.btnAsignarEstudiantes.UseVisualStyleBackColor = true;
+            // 
+            // btnProfresores
+            // 
+            this.btnProfresores.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProfresores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProfresores.Location = new System.Drawing.Point(0, 326);
+            this.btnProfresores.Name = "btnProfresores";
+            this.btnProfresores.Size = new System.Drawing.Size(200, 39);
+            this.btnProfresores.TabIndex = 5;
+            this.btnProfresores.Text = "Profesores";
+            this.btnProfresores.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProfresores.UseVisualStyleBackColor = true;
+            // 
+            // pnlSubMenuProfesores
+            // 
+            this.pnlSubMenuProfesores.Controls.Add(this.btnAsignarProfesores);
+            this.pnlSubMenuProfesores.Controls.Add(this.btnListarProfesores);
+            this.pnlSubMenuProfesores.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlSubMenuProfesores.Location = new System.Drawing.Point(0, 365);
+            this.pnlSubMenuProfesores.Name = "pnlSubMenuProfesores";
+            this.pnlSubMenuProfesores.Size = new System.Drawing.Size(200, 80);
+            this.pnlSubMenuProfesores.TabIndex = 6;
+            // 
+            // btnListarProfesores
+            // 
+            this.btnListarProfesores.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnListarProfesores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListarProfesores.Location = new System.Drawing.Point(0, 0);
+            this.btnListarProfesores.Name = "btnListarProfesores";
+            this.btnListarProfesores.Size = new System.Drawing.Size(200, 34);
+            this.btnListarProfesores.TabIndex = 1;
+            this.btnListarProfesores.Text = "Listar";
+            this.btnListarProfesores.UseVisualStyleBackColor = true;
+            // 
+            // btnAsignarProfesores
+            // 
+            this.btnAsignarProfesores.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAsignarProfesores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAsignarProfesores.Location = new System.Drawing.Point(0, 34);
+            this.btnAsignarProfesores.Name = "btnAsignarProfesores";
+            this.btnAsignarProfesores.Size = new System.Drawing.Size(200, 34);
+            this.btnAsignarProfesores.TabIndex = 2;
+            this.btnAsignarProfesores.Text = "Asignar";
+            this.btnAsignarProfesores.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(0, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Cursos";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnActividaes);
+            this.panel1.Controls.Add(this.btnListarCursos);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 485);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 80);
+            this.panel1.TabIndex = 8;
+            // 
+            // btnListarCursos
+            // 
+            this.btnListarCursos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnListarCursos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListarCursos.Location = new System.Drawing.Point(0, 0);
+            this.btnListarCursos.Name = "btnListarCursos";
+            this.btnListarCursos.Size = new System.Drawing.Size(200, 34);
+            this.btnListarCursos.TabIndex = 1;
+            this.btnListarCursos.Text = "Listar";
+            this.btnListarCursos.UseVisualStyleBackColor = true;
+            // 
+            // btnActividaes
+            // 
+            this.btnActividaes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnActividaes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnActividaes.Location = new System.Drawing.Point(0, 34);
+            this.btnActividaes.Name = "btnActividaes";
+            this.btnActividaes.Size = new System.Drawing.Size(200, 34);
+            this.btnActividaes.TabIndex = 2;
+            this.btnActividaes.Text = "Actividades";
+            this.btnActividaes.UseVisualStyleBackColor = true;
+            // 
+            // btnInformes
+            // 
+            this.btnInformes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInformes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInformes.Location = new System.Drawing.Point(0, 565);
+            this.btnInformes.Name = "btnInformes";
+            this.btnInformes.Size = new System.Drawing.Size(200, 40);
+            this.btnInformes.TabIndex = 9;
+            this.btnInformes.Text = "Informes";
+            this.btnInformes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInformes.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnCalificaciones);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 605);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 62);
+            this.panel2.TabIndex = 10;
+            // 
+            // btnCalificaciones
+            // 
+            this.btnCalificaciones.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCalificaciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalificaciones.Location = new System.Drawing.Point(0, 0);
+            this.btnCalificaciones.Name = "btnCalificaciones";
+            this.btnCalificaciones.Size = new System.Drawing.Size(200, 39);
+            this.btnCalificaciones.TabIndex = 0;
+            this.btnCalificaciones.Text = "Calificaciones";
+            this.btnCalificaciones.UseVisualStyleBackColor = true;
             // 
             // pnlSubMenu
             // 
+            this.pnlSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.pnlSubMenu.Controls.Add(this.panel2);
+            this.pnlSubMenu.Controls.Add(this.btnInformes);
+            this.pnlSubMenu.Controls.Add(this.panel1);
+            this.pnlSubMenu.Controls.Add(this.button1);
+            this.pnlSubMenu.Controls.Add(this.pnlSubMenuProfesores);
+            this.pnlSubMenu.Controls.Add(this.btnProfresores);
+            this.pnlSubMenu.Controls.Add(this.pnlSubMenuEstudiantes);
+            this.pnlSubMenu.Controls.Add(this.btnEstudiantes);
+            this.pnlSubMenu.Controls.Add(this.pnlSubMenuCursos);
+            this.pnlSubMenu.Controls.Add(this.btnUsuarios);
+            this.pnlSubMenu.Controls.Add(this.pnlLogo);
             this.pnlSubMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlSubMenu.Location = new System.Drawing.Point(0, 0);
             this.pnlSubMenu.Name = "pnlSubMenu";
-            this.pnlSubMenu.Size = new System.Drawing.Size(200, 563);
+            this.pnlSubMenu.Size = new System.Drawing.Size(200, 663);
             this.pnlSubMenu.TabIndex = 0;
+            this.pnlSubMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlSubMenu_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(970, 412);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(259, 239);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // FRMMenuUsuar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.IndianRed;
-            this.ClientSize = new System.Drawing.Size(1039, 563);
+            this.ClientSize = new System.Drawing.Size(1241, 663);
             this.Controls.Add(this.pnlContenedor);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FRMMenuUsuar";
             this.Text = "Profesores";
             this.pnlContenedor.ResumeLayout(false);
+            this.pnlLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlSubMenuCursos.ResumeLayout(false);
+            this.pnlSubMenuEstudiantes.ResumeLayout(false);
+            this.pnlSubMenuProfesores.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.pnlSubMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +350,27 @@
         #endregion
 
         private System.Windows.Forms.Panel pnlContenedor;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Panel pnlSubMenu;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnCalificaciones;
+        private System.Windows.Forms.Button btnInformes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnActividaes;
+        private System.Windows.Forms.Button btnListarCursos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlSubMenuProfesores;
+        private System.Windows.Forms.Button btnAsignarProfesores;
+        private System.Windows.Forms.Button btnListarProfesores;
+        private System.Windows.Forms.Button btnProfresores;
+        private System.Windows.Forms.Panel pnlSubMenuEstudiantes;
+        private System.Windows.Forms.Button btnAsignarEstudiantes;
+        private System.Windows.Forms.Button btnListarEstudent;
+        private System.Windows.Forms.Button btnEstudiantes;
+        private System.Windows.Forms.Panel pnlSubMenuCursos;
+        private System.Windows.Forms.Button btnListarUsuarios;
+        private System.Windows.Forms.Button btnUsuarios;
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
